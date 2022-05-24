bc934efb738f34937ebc63a6e39bd14cba12d6df
Christensenkim <Christensenkim@outlook.com>
2022-05-24 10:12:19 +0200

    asd

---
+++ DBD-synopsis-ORM Framworks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DBD_synopsis_ORM_Framworks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
